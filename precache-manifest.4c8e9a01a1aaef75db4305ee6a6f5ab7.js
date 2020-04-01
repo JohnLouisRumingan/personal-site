f52dca2eda2d9b4f22591aa2e1d1fe6d3666c2d3
John Louis Rumingan <jmrumingan@gmail.com>
2020-04-01 15:14:03 -0400

    Updates

--- precache-manifest.4c8e9a01a1aaef75db4305ee6a6f5ab7.js
+++ precache-manifest.4c8e9a01a1aaef75db4305ee6a6f5ab7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0d95a323d8db44a75f53b7478e77af3a",
+    "revision": "f3f7c27fdb78e3be4985d9abaa6e9699",
     "url": "/personal-site/index.html"
   },
   {
-    "revision": "80002539a43713e4af37",
+    "revision": "6275af4cdefc0d055db3",
     "url": "/personal-site/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "3541c23096fe2fa99d08",
-    "url": "/personal-site/static/js/2.d597228d.chunk.js"
+    "revision": "687b3983b430cb35ea5f",
+    "url": "/personal-site/static/js/2.1a3b7395.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/personal-site/static/js/2.d597228d.chunk.js.LICENSE.txt"
+    "url": "/personal-site/static/js/2.1a3b7395.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "80002539a43713e4af37",
-    "url": "/personal-site/static/js/main.a27d68a3.chunk.js"
+    "revision": "6275af4cdefc0d055db3",
+    "url": "/personal-site/static/js/main.45ad6818.chunk.js"
   },
   {
     "revision": "d4316405aebed765a751",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-site/precache-manifest.3466731517d145d995e2edd8699a3eea.js"
+  "/personal-site/precache-manifest.4c8e9a01a1aaef75db4305ee6a6f5ab7.js"
 );
 
 self.addEventListener('message', (event) => {
