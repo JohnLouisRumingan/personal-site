eb7623b09ba08447bc6f93c97437b3970434db08
John Louis Rumingan <jmrumingan@gmail.com>
2020-04-02 15:19:59 -0400

    About page working. Added note to front page.

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ class App extends React.Component {
         <br />
         Page built using CSS and HTML. Rehaul of site using React/Redux.
         <br></br>
-        {/* <p>
+        <p>
           Please note: This website is a single-page app using react-router-dom Link components.
-          However, due to github.io's method of allowing react repositories, copy pasting the 
-          URL will not actually work. 
-        </p> */}
+          However, due to github.io's method of allowing react repositories, copy/pasting the 
+          URL will not actually work to view specific links - for now. 
+        </p>
       </main>
     </div>
     );
