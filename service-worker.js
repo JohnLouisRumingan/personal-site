11d90775cf6b09811dd9da09872949c1d7be6d7e
John Louis Rumingan <jmrumingan@gmail.com>
2020-04-03 15:03:39 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-site/precache-manifest.6691aa5828e2f1e40876c8d0f5c4c47e.js"
+  "/personal-site/precache-manifest.6b485857d65339085c53710ddbdb8f4f.js"
 );
 
 self.addEventListener('message', (event) => {
