ecaf0d464295288f012fca888588ab5ff4be7142
John Louis Rumingan <jmrumingan@gmail.com>
2020-04-06 08:55:08 -0400

    Added links and icons to side drawer

--- src/Components/SideDrawer/SideDrawer.js
+++ src/Components/SideDrawer/SideDrawer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './SideDrawer.css';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux'
+import { Icon } from 'semantic-ui-react'
 
 const SideDrawer = (props) => {
 
@@ -13,8 +14,12 @@ const SideDrawer = (props) => {
     return (
         <nav className={drawerClasses.join(' ')}>
             <ul>
-                <li><Link to="/about">About</Link></li>
-                
+                {/* <li><Link to="/about">About</Link></li> */}
+                <li><Link to='/story'><Icon name="book" /><br></br>Story</Link></li> 
+                <li><Link to='/projects'><Icon name="suitcase" /><br></br>Projects</Link></li>
+                <li><Link to='/art'><Icon name="paint brush" /><br></br>Art</Link></li>
+                <li><Link to='/hobbies'><Icon name="gamepad" /><br></br>Hobbies</Link></li>
+                <li><Link to='/about'><Icon name="help" /><br></br>About</Link></li>
             </ul>
         </nav>
     )
