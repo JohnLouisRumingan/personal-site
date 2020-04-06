179720a8b0c3b715e4411e111c992a80ff0ec19b
John Louis Rumingan <jmrumingan@gmail.com>
2020-04-06 13:18:19 -0400

    Added container padding

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Run npm run deploy any time to update site
 
-Site tracker: because of how github.io works, unless I want to create multiple
-apps with the same layout under different repositories, I will have to create a
-state-based site-tracking tool. Which means no Link from react-router-dom.
+Site tracker: 
+All linking is done-client side only via React links in react-router-dom.
+Users will not be able to copy and paste links for use elsewhere.
 
 TODO: 
--- src/Containers/StoryContainer.js
+++ src/Containers/StoryContainer.js
@@ -5,7 +5,17 @@ const StoryContainer = () => {
 
     return (
         <div className='container-page'>
-            StoryContainer
+            My Story:
+
+            <p>
+                Full-stack web developer based in Washington, DC Metro Area. Passion for bringing people together 
+                through code. Fluent in JavaScript, React.js, Ruby, and Ruby on Rails. Familiar with Python, R, and C++.
+                Self-driven problem solver and effective communicator seeking to make an impact with cutting-edge technology.
+
+                Hobbies:
+                Economic and logistical board games
+                Surfing
+            </p>
         </div>
     )
 }
