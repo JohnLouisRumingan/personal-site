b5582d832a6b4bc400dd4aa85a0360cd45f019ce
John Louis Rumingan <jmrumingan@gmail.com>
2020-04-07 08:28:14 -0400

    Added note about client-side routing in about page.

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,9 +9,8 @@ const About = () => {
             <br></br>
             <p>
             Page built using CSS and HTML. Rehaul of site using React/Redux.
-            Please note: This website is a single-page app using react-router-dom Link components.
-            However, due to github.io's method of allowing react repositories, copy/pasting the 
-            URL will not actually work to view specific links - for now. 
+            Please note: This website is a single-page app using react-router-dom components.
+            Copy/pasting URLs will not work - all routing is done client-side. 
             </p>
         </div>
     )
