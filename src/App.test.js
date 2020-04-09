700d6f2a7dd8fdb68baf2bb53d0e7a20cf9d3c32
John Louis Rumingan <jmrumingan@gmail.com>
2020-04-09 13:03:26 -0400

    Working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import ArtContainer from './Containers/ArtContainer'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('art container says no commissions', () => {
+
+  const { getByText } = render(<ArtContainer />);
+  const noCommission = getByText(/Currently not accepting commissions./);
+  expect(noCommission).toBeInTheDocument();
+})
