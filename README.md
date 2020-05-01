1c260463ea56f6807206a6df1600ae1cbf43e5cd
John Louis Rumingan <jmrumingan@gmail.com>
2020-05-01 16:51:05 -0400

    Add hobby container test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Run npm run deploy any time to update site
 
-Site tracker: 
+Note:
 All linking is done-client side only via React links in react-router-dom.
 Users will not be able to copy and paste links for use elsewhere.
 
--- src/Containers/Containers.test.js
+++ src/Containers/Containers.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import ArtContainer from './ArtContainer'
+import ArtContainer from './ArtContainer';
+import HobbyContainer from './HobbyContainer';
 
 test('art container says no commissions', () => {
 
   const { getByText } = render(<ArtContainer />);
   const noCommission = getByText(/Currently not accepting commissions./);
   expect(noCommission).toBeInTheDocument();
+})
+
+test('hobby container contains "Hobbies" ', () => {
+
+  const { getByText } = render(<HobbyContainer />);
+  const hobby = getByText(/Hobbies:/);
+  expect(hobby).toBeInTheDocument();
 })
