f575fd80b924b3ca806cde6097d9df8b3100c7b4
John Louis Rumingan <jmrumingan@gmail.com>
2020-08-23 12:18:28 -0400

    Add URL behavior to about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,7 +10,7 @@ const About = () => {
             <p>
             Page built using CSS and HTML. Rehaul of site using React/Redux.
             Please note: This website is a single-page app using react-router-dom components.
-            Copy/pasting URLs will not work - all routing is done client-side. 
+            Copy/pasting URLs aside from /personal-site will not work - all routing is done client-side. 
             </p>
             <p>
             Background photograph by Kevin Ku.
