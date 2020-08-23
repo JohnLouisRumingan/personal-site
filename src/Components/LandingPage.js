bb7db6d83c85eb450097d4e3c6b14b41fbbf4a5a
John Louis Rumingan <jmrumingan@gmail.com>
2020-08-23 12:30:59 -0400

    Remove old code from landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -35,33 +35,6 @@ const LandingPage = () => {
                     <Icon name='medium' size='small'/>
                 </a>
             </div>
-
-
-
-            <div className='landing-page'>
-                <div className='sub-icon-background'>
-                    <div className='sub-icon-box'>
-                        <a href='https://github.com/JohnLouisRumingan'
-                            onClick={(e) => {
-                                e.preventDefault();
-                                window.open("https://github.com/JohnLouisRumingan");}}>
-                            <Icon name='github' size='small' />
-                        </a>
-                        <a href='https://www.linkedin.com/in/john-louis-rumingan/'
-                            onClick={(e) => {
-                                e.preventDefault();
-                                window.open('https://www.linkedin.com/in/john-louis-rumingan/')}}>
-                            <Icon name='linkedin' size='small'/>
-                        </a>
-                        <a href='https://medium.com/@john.louis.rumingan'
-                            onClick={(e) => {
-                                e.preventDefault();
-                                window.open('https://medium.com/@john.louis.rumingan')}}>
-                            <Icon name='medium' size='small'/>
-                        </a>
-                    </div>
-                </div>
-            </div>
         </main>
     )
 }
