696c4db56d7c911d4dcac25ea12a2da9ce873f32
John Louis Rumingan <jmrumingan@gmail.com>
2020-08-24 15:11:11 -0400

    Change projects page, made responsive for mobile

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -6,26 +6,42 @@ const ProjectCard = (props) => {
     let {name, description, url, imgSource} = props.info
 
     return (
-        <div className='project-box'>
-            <Card>
-                {imgSource? <Image src={imgSource} wrapped ui={false}/> : null }
-                <Card.Header>
-                    <h2>{name}</h2>
-                </Card.Header>
-                <br></br>
-                <Button onClick={(e) => {
-                    window.open(url)
-                }}>
-                    <Icon name='github'/>
-                    View this project on Github
-                </Button>
-                <br></br>
-                <Card.Description>
-                    <div className='scrollable-description'>
-                    {description}
-                    </div>
-                </Card.Description>
-            </Card>
+        // <div className='project-box'>
+        //     <Card>
+        //         {imgSource? <Image src={imgSource} wrapped ui={false}/> : null }
+        //         <Card.Header>
+        //             <h2>{name}</h2>
+        //         </Card.Header>
+        //         <br></br>
+        //         <Button onClick={(e) => {
+        //             window.open(url)
+        //         }}>
+        //             <Icon name='github'/>
+        //             View this project on Github
+        //         </Button>
+        //         <br></br>
+        //         <Card.Description>
+        //             <div className='scrollable-description'>
+        //             {description}
+        //             </div>
+        //         </Card.Description>
+        //     </Card>
+        // </div>
+
+        <div className="item">
+            <a href={url} onClick={(e) => {
+                        e.preventDefault();
+                        window.open(url);
+                        }}>
+                <img src={imgSource} alt={name}></img>
+            </a>
+            <a href="#" className="btn-light">
+                <i className="fas fa-eye"></i>
+                <Icon name='eye'/> Project {name}
+            </a>
+            <a href="#" className="btn-dark">
+                <Icon name='github' /> Github
+            </a>
         </div>
     )
 }
--- src/Containers/ProjectContainer.js
+++ src/Containers/ProjectContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Container.css'
+// import './Container.css'
 import { connect } from 'react-redux'
 import { Card } from 'semantic-ui-react'
 import ProjectCard from '../Components/ProjectCard'
@@ -7,14 +7,26 @@ import ProjectCard from '../Components/ProjectCard'
 const ProjectContainer = (props) => {
 
     return (
-        <div className='container-page'>
-            <div className='container-banner'>
-            Projects page:
+        // <div className='container-page'>
+        //     <div className='container-banner'>
+        //     Projects page:
+        //     </div>
+        //     <Card.Group>
+        //         {props.projects? props.projects.map( project => <ProjectCard info={project}/>) : null }
+        //     </Card.Group>
+        // </div>
+
+        <main id="work">
+            <h1 className="lg-heading">
+                My <span class="text-secondary">Work</span>
+            </h1>
+            <h2 className="sm-heading">
+                Some projects I've made
+            </h2>
+            <div className="projects">
+                {props.projects? props.projects.map(project => <ProjectCard info={project}/>) : null }
             </div>
-            <Card.Group>
-                {props.projects? props.projects.map( project => <ProjectCard info={project}/>) : null }
-            </Card.Group>
-        </div>
+        </main>
     )
 }
 
