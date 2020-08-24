408edee1b24c11a36bc693d746eca99da4e25be8
John Louis Rumingan <jmrumingan@gmail.com>
2020-08-23 20:26:27 -0400

    Add footer css. Still requires sticky footer behavior

--- src/Containers/StoryContainer.js
+++ src/Containers/StoryContainer.js
@@ -8,9 +8,9 @@ const StoryContainer = () => {
         //     <div>
         //         <h1>My Story:</h1>
         //         <p>
-        //             Full-stack web developer based in Washington, DC Metro Area. Passion for bringing people together 
-        //             through code. Fluent in JavaScript, React.js, Ruby, and Ruby on Rails.
-        //             Self-driven problem solver and effective communicator seeking to make an impact with cutting-edge technology.
+                    // Full-stack web developer based in Washington, DC Metro Area. Passion for bringing people together 
+                    // through code. Fluent in JavaScript, React.js, Ruby, and Ruby on Rails.
+                    // Self-driven problem solver and effective communicator seeking to make an impact with cutting-edge technology.
         //         </p>
         //     </div>
         // </div>
@@ -26,25 +26,27 @@ const StoryContainer = () => {
                 <div className="bio">
                     <h3 class="text-secondary">Bio</h3>
                     <p>
-                        Lorem ipsum
+                    Full-stack web developer based in Washington, DC Metro Area. Passion for bringing people together 
+                    through code. Fluent in JavaScript, React.js, Ruby, and Ruby on Rails.
+                    Self-driven problem solver and effective communicator seeking to make an impact with cutting-edge technology.
                     </p>
                 </div>
-                <div className="job job-1">
+                <div className="job past-1">
                     <h3>Experience</h3>
                     <p>
                         Story
                     </p>
                 </div>
-                <div className="job job-2">
+                <div className="job techstack-2">
                     <h3>Technology I know</h3>
                     <p>
                         List of tech here
                     </p>
                 </div>
-                <div className="job job-3">
+                <div className="job student-3">
                     <h3>Full-Stack Development Student</h3>
                     <p>
-                        
+
                     </p>
                 </div>
             </div>
