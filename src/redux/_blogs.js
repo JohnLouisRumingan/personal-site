b9170089a13251b3210b9040f0072593a0eb8d7a
John Louis Rumingan <55402017+JohnLouisRumingan@users.noreply.github.com>
2022-01-07 16:53:44 -0500

    Update blogPost order

--- src/redux/_blogs.js
+++ src/redux/_blogs.js
@@ -2,6 +2,11 @@
 
 const blogPosts = [
     {
+        title: "How to add a second remote",
+        url: "https://medium.com/@john.louis.rumingan/how-to-add-a-second-remote-to-a-local-repository-7d0e36f18d58",
+        description: "Short how-to about adding remotes to local repository for certain use cases"
+    },
+    {
         title: "Gaining insight from writing tests",
         url: "https://medium.com/@john.louis.rumingan/action-objects-and-reducers-when-writing-tests-lead-to-insights-2703df97ad4e?sk=e10cfd730365bc423918f9cc8dea7680",
         description: "How writing tests help you understand your own code better"
@@ -10,11 +15,6 @@ const blogPosts = [
         title: "Throwing, Catching, and Unforced Errors",
         url: "https://medium.com/@john.louis.rumingan/throwing-catching-and-unforced-errors-f8f70960d3b9",
         description: "Looking at past projects more critically"
-    },
-    {
-        title: "How to add a second remote",
-        url: "https://medium.com/@john.louis.rumingan/how-to-add-a-second-remote-to-a-local-repository-7d0e36f18d58",
-        description: "Short how-to about adding remotes to local repository for certain use cases"
     },
     {
         title: "Updating My Portfolio Site",
